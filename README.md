71b81b0a8dc78526e378b961ec8792f300eb65b7
Azaria <askelman5@gmail.com>
2024-10-31 22:32:15 -0400

    Pasted files from old repo

---
+++ README.md
@@ -0,0 +1,22 @@
+# SmartMeal
+## Recipe generation, recipe storage, and nutrient management 
+
+
+### Contributors
+
+#### Nachuan Wang (December-17th)
+As a user, John wants to generate recipes from the ingredients he had on hand.
+
+#### Azaria Kelman (azariak)
+User story...
+
+#### Anisa (AHK()
+Sara previously generated her favourite recipe, but
+forgot what the ingredients were. She opens the SmartMeal program and 
+chooses to load her saved recipes. Her previously saved recipes are displayed 
+based on their rank, along with the ingredients, calories, and nutritional benefits 
+of each.
+
+#### Daniel Rafailov (danielrafailov1)
+Emma needs to meal prep for the week. She opens the meal planning program, selects
+her dietary preferences and available ingredients and receives a personalized meal plan with recipes.
---
+++ src/main/java/app/Main.java
@@ -0,0 +1,30 @@
+package app;
+
+import javax.swing.JFrame;
+
+/**
+ * The Main class of our application.
+ */
+public class Main {
+    /**
+     * Builds and runs the CA architecture of the application.
+     * @param args unused arguments
+     */
+    public static void main(String[] args) {
+        final AppBuilder appBuilder = new AppBuilder();
+        // TODO: add the Logout Use Case to the app using the appBuilder DONE
+        final JFrame application = appBuilder
+                                            .addLoginView()
+                                            .addSignupView()
+                                            .addLoggedInView()
+                                            .addSignupUseCase()
+                                            .addLoginUseCase()
+                                            .addChangePasswordUseCase()
+                                            .addLogoutUseCase()
+                                            // added this line above
+                                            .build();
+
+        application.pack();
+        application.setVisible(true);
+    }
+}
