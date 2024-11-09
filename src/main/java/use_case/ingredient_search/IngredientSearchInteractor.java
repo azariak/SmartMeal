40e9cf96757dcc14e05aad287e1d57fcafd5f774
nachuan <wangnac1210@gmail.com>
2024-11-09 00:16:20 -0500

    some more incomplete implementation of ingredient search related functions.

--- src/main/java/use_case/ingredient_search/IngredientSearchInteractor.java
+++ src/main/java/use_case/ingredient_search/IngredientSearchInteractor.java
@@ -1,4 +1,22 @@
 package use_case.ingredient_search;
 
-public class IngredientSearchInteractor {
+public class IngredientSearchInteractor implements IngredientSearchInputBoundary {
+    private final IngredientSearchDataAccessInterface ingredientSearchDataAccessObject;
+    private final IngredientSearchOutputBoundary ingredientSearchPresenter;
+
+    public IngredientSearchInteractor(IngredientSearchDataAccessInterface ingredientSearchDataAccessObject,
+                                      IngredientSearchOutputBoundary ingredientSearchOutputBoundary) {
+
+        this.ingredientSearchDataAccessObject = ingredientSearchDataAccessObject;
+        this.ingredientSearchPresenter = ingredientSearchOutputBoundary;
+    }
+
+    @Override
+    public void execute(IngredientSearchInputData ingredientSearchInputData) {
+        final String ingredient1 = ingredientSearchInputData.getIngredient1();
+        final String ingredient2 = ingredientSearchInputData.getIngredient2();
+        final String ingredient3 = ingredientSearchInputData.getIngredient3();
+
+        ingredientSearchPresenter.prepareResultView(ingredient1, ingredient2, ingredient3);
+        }
 }
