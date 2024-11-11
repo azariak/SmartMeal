9a26532d1ae17af290b840560dd90297bfe4be4f
Azaria <askelman5@gmail.com>
2024-11-10 21:30:01 -0500

    added file

---
+++ src/main/java/entity/RecipeIDToJSON.java
@@ -0,0 +1,10 @@
+package entity;
+
+/**
+ * This class takes a given recipe ID and returns a JSON of Recipe information.
+ */
+public class RecipeIDToJSON {
+    public static void main(String[] args) {
+
+    }
+}
