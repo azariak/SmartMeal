382a14094deb93b3005e96ab586c175bca17313a
Azaria <askelman5@gmail.com>
2024-11-11 10:36:11 -0500

    Moved file

--- src/main/java/data_access/RecipeIDToJSON.java
+++ src/main/java/data_access/RecipeIDToJSON.java
@@ -1,4 +1,4 @@
-package entity;
+package data_access;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,22 +7,16 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * This class returns a list of recipes for the given ingredients.
+ * This class takes a given recipe ID and returns a JSON of Recipe information.
  */
-public class IngredientsToRecipeID {
+public class RecipeIDToJSON {
 
-    // Note: FOR EACH RECIPE, YOU NEED ANOTHER API CALL FOR THE RECIPE DETAILS.
-
-    // Store the API key as a constant in your code. DO NOT PUSH TO GIT.
-    /**
-     * This class returns a list of recipes for the given ingredients.
-     */
     public static void main(String[] args) {
 
-        String ingredients = "cheese, pasta";
-        String urlString = "https://api.spoonacular.com/recipes/complexSearch?apiKey="
-                + System.getenv("API_KEY") + "&includeIngredients=" + ingredients + "&number=10";
-
+        final String id = "654959"; // Sample ID for Pasta with Tuna
+//        String urlString = "https://api.spoonacular.com/recipes/complexSearch?apiKey="
+//                + System.getenv("API_KEY") + "{" + id + "}" + "/information";
+        String urlString = "https://api.spoonacular.com/recipes/716429/information?includeNutrition=false";
 
         try {
             // Make a GET request to the API
@@ -47,14 +41,14 @@ public class IngredientsToRecipeID {
 
                 // Output the response (for now, just print it)
                 System.out.println(response.toString());
-            }
-            else {
+            } else {
                 System.out.println("GET request failed. Response Code: " + responseCode);
             }
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
