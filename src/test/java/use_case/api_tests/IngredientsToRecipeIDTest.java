bbb14e5a07096dd84a655088a000a232a2bb88c0
Azaria <askelman5@gmail.com>
2024-11-11 20:16:03 -0500

    Fixed CheckStyle

--- src/test/java/use_case/api_tests/IngredientsToRecipeIDTest.java
+++ src/test/java/use_case/api_tests/IngredientsToRecipeIDTest.java
@@ -1,17 +1,8 @@
 package use_case.api_tests;
 
-import data_access.InMemoryUserDataAccessObject;
-import entity.CommonUserFactory;
-import entity.User;
-import entity.UserFactory;
 import org.junit.jupiter.api.Test;
-import use_case.login.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import data_access.IngredientsToRecipeID;
-import java.util.HashMap;
 import java.util.Map;
 
 class IngredientsToRecipeIDTest {
