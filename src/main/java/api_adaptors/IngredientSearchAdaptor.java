761af126b826cf060548664fe01a9ed0a40648dd
Nachuan <wangnac1210@gmail.com>
2024-11-14 17:53:43 -0500

    Implemented logic for passing ingredient inputs to api adaptor

--- src/main/java/api_adaptors/IngredientSearchAdaptor.java
+++ src/main/java/api_adaptors/IngredientSearchAdaptor.java
@@ -1,13 +1,28 @@
 package api_adaptors;
 
+import java.util.ArrayList;
+
 /**
  * The adaptor that converts ingredient search output to api call.
  */
 public class IngredientSearchAdaptor implements IngredientSearchAdaptorBoundary {
     @Override
     public void ingredientSearchToApiCall(String ingredient1, String ingredient2, String ingredient3) {
-        final String ingrdients = ingredient1 + ", " + ingredient2 + ", " + ingredient3;
-        System.out.println(ingrdients);
+
+        final ArrayList<String> ingredientsArray = new ArrayList<>();
+
+        if (!"".equals(ingredient1)) {
+            ingredientsArray.add(ingredient1);
+        }
+        if (!"".equals(ingredient2)) {
+            ingredientsArray.add(ingredient2);
+        }
+        if (!"".equals(ingredient3)) {
+            ingredientsArray.add(ingredient3);
+        }
+
+        final String output = String.join(",", ingredientsArray);
+        System.out.println(output);
     }
 
 }
