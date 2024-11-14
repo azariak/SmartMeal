b3490e5f09c9b789e1d3245176140fb05ae335e6
Nachuan <wangnac1210@gmail.com>
2024-11-14 16:15:42 -0500

    Implemented

--- src/main/java/api_adaptors/IngredientSearchAdaptor.java
+++ src/main/java/api_adaptors/IngredientSearchAdaptor.java
@@ -6,7 +6,7 @@ package api_adaptors;
 public class IngredientSearchAdaptor implements IngredientSearchAdaptorBoundary {
     @Override
     public void ingredientSearchToApiCall(String ingredient1, String ingredient2, String ingredient3) {
-
+        final String ingrdients = ingredient1 + ", " + ingredient2 + ", " + ingredient3;
     }
 
 }
