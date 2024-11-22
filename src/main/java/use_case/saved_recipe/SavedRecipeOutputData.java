3cf222e6a7d375f19cd3cfc569c83f017859614a
anisa <anisa.hosseinpourkhoshghalb@mail.utoronto.ca>
2024-11-22 09:14:42 -0500

    changed some files regarding saved use case

--- src/main/java/use_case/saved_recipe/SavedRecipeOutputData.java
+++ src/main/java/use_case/saved_recipe/SavedRecipeOutputData.java
@@ -1,4 +1,11 @@
 package use_case.saved_recipe;
 
+/**
+ * Output Data for the saved recipe Use Case.
+ */
 public class SavedRecipeOutputData {
+//
+//    private final String recipeId;
+//    private final String recipeName;
+
 }
