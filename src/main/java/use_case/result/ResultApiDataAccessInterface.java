5d09df62b07944923246b4b9531abf8e1a17ec24
Nachuan <wangnac1210@gmail.com>
2024-11-24 16:46:16 -0500

    Partial implementation of logic for second api call.

--- src/main/java/use_case/result/ResultApiDataAccessInterface.java
+++ src/main/java/use_case/result/ResultApiDataAccessInterface.java
@@ -4,4 +4,11 @@ package use_case.result;
  * Data access interface for result use case.
  */
 public interface ResultApiDataAccessInterface {
+
+    /**
+     * Excute the api call for recipe details.
+     * @param id the id of the recipe.
+     */
+    void execute(String id);
+
 }
