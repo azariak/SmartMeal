4a400896188347f7980efb5100f7fa461e978a83
bentenroadrunner <danielrafailov180@gmail.com>
2024-11-23 20:46:50 -0500

    RecipeDetailView almost done

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,15 +1,12 @@
-package view;
-
-import interface_adapter.load_saved_recipe.LoadSavedRecipeController;
-import interface_adapter.result.ResultController;
-import interface_adapter.result.ResultViewModel;
-
-public class ResultView {
-    private final String frameName = "SmartMeal: Recipes";
-    private ResultViewModel resultViewModel;
-    private ResultController resultController;
-
-    private final
-
-}
+//package view;
+//
+//import interface_adapter.load_saved_recipe.LoadSavedRecipeController;
+//import interface_adapter.result.ResultController;
+//import interface_adapter.result.ResultViewModel;
+//
+//public class ResultView {
+//    private final String frameName = "SmartMeal: Recipes";
+//    private ResultViewModel resultViewModel;
+//    private ResultController resultController;
+//}
 
