d64f4633affe9c4852a0ee88bc53f189a1a26974
Nachuan <wangnac1210@gmail.com>
2024-11-24 15:28:45 -0500

    Partial implementation of logic for second api call.

--- src/main/java/use_case/result/ResultInputBoundary.java
+++ src/main/java/use_case/result/ResultInputBoundary.java
@@ -1,7 +1,15 @@
 package use_case.result;
 
+import entity.GenericRecipe;
+
 /**
  * The input boundary for the result use case.
  */
 public interface ResultInputBoundary {
+
+    /**
+     * Execute a api call.
+     * @param recipe the recipe to search.
+     */
+    void execute(GenericRecipe recipe);
 }
