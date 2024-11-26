0607db7789bb42422371b37dd2a29aed3cb5f9a8
bentenroadrunner <danielrafailov180@gmail.com>
2024-11-26 12:48:37 -0500

    more debugging

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -23,10 +23,10 @@ public class Main {
                                             .addRankedView()
                                             // added this line above
                                             .addResultView()
+                                            .addRecipeDetailView()
                                             .addResultUseCase()
                                             .addIngredientSearchView()
                                             .addIngredientSearchUseCase()
-                                            .addRecipeDetailView()
                                             .addRecipeDetailUseCase()
                                             .addLoadSavedRecipeView()
                                             .addLoadSavedRecipeUseCase()
