fb877448b551dee9db512b0660122859b2ff75ee
Nachuan <wangnac1210@gmail.com>
2024-11-27 01:31:25 -0500

    Changed the implementation of back function in ViewModel to account for more views.

--- src/main/java/interface_adapter/ViewManagerModel.java
+++ src/main/java/interface_adapter/ViewManagerModel.java
@@ -1,10 +1,13 @@
 package interface_adapter;
 
+import java.util.ArrayList;
+
 /**
  * Model for the View Manager. Its state is the name of the View which
  * is currently active. An initial state of "" is used.
  */
 public class ViewManagerModel extends ViewModel<String> {
+    private final ArrayList<String> viewHistory = new ArrayList<>();
 
     public ViewManagerModel() {
         super("view manager");
@@ -16,7 +19,26 @@ public class ViewManagerModel extends ViewModel<String> {
      * @param view view.
      */
     public void switchView(String view) {
-        this.setPreviousViewName(this.getState());
+        this.addViewHistory(this.getState());
         this.setState(view);
+    }
+
+    /**
+     * Retrieve the name of the last view and remove it from the history.
+     * @return the name of the last view.
+     */
+    public String getPreviousViewName() {
+        final String result = this.viewHistory.get(0);
+        viewHistory.remove(0);
+        return result;
+    }
+
+    /**
+     * Add View name to view history.
+     * @param name view name.
+     */
+    public void addViewHistory(String name) {
+        this.viewHistory.add(0, name);
+
     }
 }
