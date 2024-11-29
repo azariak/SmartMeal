a43cc57692bcc5a759bdf2166e22c971c8803352
Azaria <askelman5@gmail.com>
2024-11-29 11:55:36 -0500

    minor changes

--- src/main/java/interface_adapter/main_menu/MainMenuViewModel.java
+++ src/main/java/interface_adapter/main_menu/MainMenuViewModel.java
@@ -6,8 +6,8 @@ import interface_adapter.ViewModel;
  * The view model for main menu view.
  */
 public class MainMenuViewModel extends ViewModel<MainMenuState> {
-    public static final String TITLE_LABEL = "                        Smart Meal";
-    public static final String TO_LOGIN_BUTTON_LABEL = "To Login";
+    public static final String TITLE_LABEL = "                        " + "SmartMeal";
+    public static final String TO_LOGIN_BUTTON_LABEL = "Login";
     public static final String SIGNUP_BUTTON_LABEL = "Sign Up";
 
     public MainMenuViewModel() {
