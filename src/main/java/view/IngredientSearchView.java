1efdae71ad6782459d57a396c45d2ad49e059357
bentenroadrunner <danielrafailov180@gmail.com>
2024-12-01 20:21:25 -0500

    fixed weird loading screen bug

--- src/main/java/view/IngredientSearchView.java
+++ src/main/java/view/IngredientSearchView.java
@@ -13,7 +13,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -48,13 +47,9 @@ public class IngredientSearchView extends JPanel implements ActionListener, Prop
 
     private final JPanel ingredientSearchPanel;
     private final JPanel buttons;
-    private final JPanel loadingPanel;
 
     private JButton search;
     private JButton cancel;
-
-    // JLabel for loading indicator
-    private JLabel loadingLabel;
 
     public IngredientSearchView(IngredientSearchViewModel ingredientSearchViewModel) {
 
@@ -64,11 +59,9 @@ public class IngredientSearchView extends JPanel implements ActionListener, Prop
 
         this.ingredientSearchPanel = new JPanel();
         this.buttons = new JPanel();
-        this.loadingPanel = new JPanel();  // Panel to hold loading indicator
 
         addIngredientSearchPanel();
         addButtons();
-        addLoadingIndicator();  // Add the loading indicator to the view
     }
 
     private void addButtons() {
@@ -104,12 +97,10 @@ public class IngredientSearchView extends JPanel implements ActionListener, Prop
                     ingredients.add(currentState.getIngredient2());
                     ingredients.add(currentState.getIngredient3());
 
-                    // Show loading indicator when search starts
-                    showLoadingIndicator();
-
                     try {
                         ingredientSearchController.execute(ingredients);
-                    } catch (IOException exception) {
+                    }
+                    catch (IOException exception) {
                         throw new RuntimeException(exception);
                     }
                 }
@@ -214,36 +205,6 @@ public class IngredientSearchView extends JPanel implements ActionListener, Prop
             public void changedUpdate(DocumentEvent e) {
                 documentListenerHelper();
             }
-        });
-    }
-
-    private void addLoadingIndicator() {
-        // Create and configure the loading indicator (JLabel)
-        loadingLabel = new JLabel("Loading...");
-        loadingLabel.setVisible(false);  // Hide it initially
-
-        // Add the loading indicator to the loadingPanel
-        loadingPanel.add(loadingLabel);
-
-        // Add the loadingPanel to the main panel
-        this.add(loadingPanel);
-    }
-
-    private void showLoadingIndicator() {
-        // Show the loading label and hide other components
-        SwingUtilities.invokeLater(() -> {
-            loadingLabel.setVisible(true);
-            ingredientSearchPanel.setVisible(false);
-            buttons.setVisible(false);
-        });
-    }
-
-    private void hideLoadingIndicator() {
-        // Hide the loading label and show other components
-        SwingUtilities.invokeLater(() -> {
-            loadingLabel.setVisible(false);
-            ingredientSearchPanel.setVisible(true);
-            buttons.setVisible(true);
         });
     }
 
